87ddef841e02472ba5bf7934721a37d532a35eac
JAOUAD BRAOUZ <jbraouz@holged.local>
2026-09-25 15:46:42 +0000

    Initial WorkPilot setup

---
+++ database/factories/WorkSessionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\WorkSession;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<WorkSession>
+ */
+class WorkSessionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
